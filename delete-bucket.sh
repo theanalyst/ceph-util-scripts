96b55e9abc38971db6925ee7f16791ad036e2130
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-04-07 14:02:32 +0530

    Adding delete-bucket.sh
    Script to delete bucket and all its contents

---
+++ delete-bucket.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# deletes a bucket and all objects in it
+
+bucket=$1
+buckets=$(s3 -us list $bucket | awk '{print $1}' | tail -n +4)
+
+for i in $buckets
+do
+    s3 -us delete $bucket/$i
+done
+
+    
